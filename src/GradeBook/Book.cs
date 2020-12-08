33025378efed095238db7a48e60f56f4e9734937
Matthew Reeds <mattlreeds@gmail.com>
2020-12-07 21:28:55 -0600

    created try catch and finally for user input grades

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -45,7 +45,7 @@ namespace GradeBook
             }
             else
             {
-                Console.WriteLine("Invalid value");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
             }
         }
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,12 +8,39 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Nolan's Grade Book");
-            book.AddGrade(78.2);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit.");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
+
+
+
             var stats = book.GetStats();
 
-            Console.WriteLine($" The highest grade is {stats.High:N1}\n " +
+            Console.WriteLine($"" +
+                $"The highest grade is {stats.High:N1}\n " +
                 $"The average grade is {stats.Average:N1}\n " +
                 $"The lowest grade is {stats.Low:N1}\n " +
                 $"The letter grade is {stats.Letter}");
