535c551c53011574c2cca7ef829c8b976066daa9
Matthew Reeds <mattlreeds@gmail.com>
2020-12-08 12:48:14 -0600

    Further abstracted statistics calculations

--- src/GradeBook/InMemoryBook.cs
+++ src/GradeBook/InMemoryBook.cs
@@ -18,7 +18,14 @@ namespace GradeBook
         }
     }
 
-    public abstract class Book : NamedObject
+    public interface IBook
+    {
+        void AddGrade(double grade);
+        Statistics GetStats();
+        string Name { get; }
+    }
+
+    public abstract class Book : NamedObject, IBook
     {
         public Book(string name) : base(name)
         {
@@ -26,6 +33,11 @@ namespace GradeBook
         }
 
         public abstract void AddGrade(double grade);
+
+        public virtual Statistics GetStats()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class InMemoryBook : Book
@@ -74,59 +86,16 @@ namespace GradeBook
             }
         }
 
-        public Statistics GetStats()
+        public override Statistics GetStats()
         {
             var result = new Statistics();
-            result.Average = 0.0;
-            result.High = double.MinValue;
-            result.Low = double.MaxValue;
 
-            foreach (var grade in grades)
+            for (var index = 0; index < grades.Count; index += 1)
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
+                result.Add(grades[index]);
             }
 
-            //for (var index = 0; index < grades.Count; index++)
-            //{
-            //    if(grades[index] == 42.1)
-            //    {
-            //        break;
-            //        continue;
-            //    }
-            //    result.High = Math.Max(grades[index], result.High);
-            //    result.Low = Math.Min(grades[index], result.Low);
-            //    result.Average += grades[index];
-            //}
-
-            result.Average /= grades.Count;
-
-
-            switch (result.Average)
-            {
-                case var d when d > 90.0:
-                    result.Letter = 'A';
-                    break;
-
-                case var d when d >= 80.0:
-                    result.Letter = 'B';
-                    break;
-
-                case var d when d > 70.0:
-                    result.Letter = 'C';
-                    break;
-
-                case var d when d > 60.0:
-                    result.Letter = 'D';
-                    break;
-
-                default:
-                    result.Letter = 'F';
-                    break;
-            }
             return result;
-
         }
 
         private List<double> grades;
